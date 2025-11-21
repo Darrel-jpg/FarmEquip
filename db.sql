07df5d67f2753fd210cb4fc8ed0f6f27da2b4c8a
Syadid <Syadidchoiri@gmail.com>
2025-11-21 17:27:22 +0700

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE alatpertanian (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nama VARCHAR(255) NOT NULL,
+    harga INT NOT NULL,
+    kategori VARCHAR(255) NOT NULL,
+    fotourl LONGTEXT NOT NULL
+);
